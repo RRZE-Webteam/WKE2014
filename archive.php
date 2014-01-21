752131ee84053221f2e81430d0030a0844c5cad6
Barbara Bothe <barbara.bothe@fau.de>
2014-01-21 13:11:34 +0100

    Styling für Kalender-Widget
    Styling für Widget, Anpassung Seitentitel für Kalender-Archiv-Seiten
    (Tag, Monat, Jahr)

--- archive.php
+++ archive.php
@@ -1,11 +1,43 @@
-<?php 
-    get_header();    
-    global $options;  
- 
-            rewind_posts(); 
-            get_template_part( 'loop', 'archive' ); 
-            ?>
+<?php
+/**
+ * Template for displaying Archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+				<?php
+					rewind_posts();
+					get_template_part( 'loop', 'archive' );
+
+				?>
 
 
+			<?php else : ?>
 
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'wke2014' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'wke2014' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -17,26 +17,26 @@ global $options;
 </head>
 
 <body <?php body_class(); ?>>  <!-- begin: body -->
-  <div class="page_margins">  <!-- begin: page_margins --> 
+  <div class="page_margins">  <!-- begin: page_margins -->
     <div id="seite" class="page">  <!-- begin: seite -->
       <a name="seitenmarke" id="seitenmarke"></a>
-      
-      <header><div id="kopf">  <!-- begin: kopf -->   
-	    
-	     
+
+      <header><div id="kopf">  <!-- begin: kopf -->
+
+
 	    <div id="logo">
-		    <p>				
+		    <p>
 		    <?php if ( ! is_home() ) { ?>
 			<a href="<?php echo home_url( '/' ); ?>" title="<?php echo $options['default_text_title_home_backlink']; ?>" rel="home" class="logo">
-		    <?php } ?>                                                             
+		    <?php } ?>
 		    <img src="<?php header_image(); ?>" alt="<?php bloginfo( 'name' ); ?>">
-		<?php                                
+		<?php
 		if ( ! is_home() ) { ?> </a>  <?php } ?>
 		    </p>
 
 	    </div>
-	    <?php if ( is_active_sidebar( 'headerbox-area' ) ) { 
-		dynamic_sidebar( 'headerbox-area' ); 
+	    <?php if ( is_active_sidebar( 'headerbox-area' ) ) {
+		dynamic_sidebar( 'headerbox-area' );
 	     } elseif (isset($options['headerbox-datum']) && isset($options['headerbox-title'])) { ?>
 		 <div id="header-box">
 		    <div class="wrapper">
@@ -49,31 +49,41 @@ global $options;
 	    <?php get_search_form(); ?>
 
 	    <div id="titel">
-		    <h1><?php wke2014_contenttitle(); ?></h1> 
-	    </div>       
+		    <h1>
+				<?php if ( is_day() ) : ?>
+					<?php printf( __( '%s', 'wke2014' ), '<span>' . get_the_date() . '</span>' ); ?>
+				<?php elseif ( is_month() ) : ?>
+					<?php printf( __( '%s', 'wke2014' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'wke2014' ) ) . '</span>' ); ?>
+				<?php elseif ( is_year() ) : ?>
+					<?php printf( __( '%s', 'wke2014' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'wke2014' ) ) . '</span>' ); ?>
+				<?php else : ?>
+					<?php wke2014_contenttitle(); ?>
+				<?php endif; ?>
+			</h1>
+	    </div>
 
-	      
-			
-		 
+
+
+
 	    <div id="breadcrumb">
 		    <h2>Sie befinden sich hier: </h2>
-			    <p>			    
+			    <p>
 				<?php if (function_exists('wke2014_breadcrumbs')) wke2014_breadcrumbs(); ?>
 			    </p>
-	    </div>               
+	    </div>
 	    <div id="sprungmarken">
 	      <h2>Sprungmarken</h2>
 	      <ul>
 			<li class="first"><a href="#contentmarke">Zum Inhalt</a><span class="skip">. </span></li>
-			<li><a href="#bereichsmenumarke">Zur Navigation</a><span class="skip">. </span></li>		
-			<li class="last"><a href="#sidebar">Zu sonstigen Informationen</a><span class="skip">. </span></li>            
+			<li><a href="#bereichsmenumarke">Zur Navigation</a><span class="skip">. </span></li>
+			<li class="last"><a href="#sidebar">Zu sonstigen Informationen</a><span class="skip">. </span></li>
 	      </ul>
 	    </div>
 	    <?php if ( has_nav_menu( 'targetmenue' ) ) { ?>
 		<nav id="zielgruppen-menue" class="zielgruppen-menue" role="navigation">
 		    <?php wp_nav_menu( array( 'theme_location' => 'targetmenue', 'fallback_cb' => '', 'depth' => 1 ) );?>
 		</nav><!-- #target-navigation -->
-	     <?php } ?> 	  
+	     <?php } ?>
 		 <?php if ($options['aktiv-slider']==1) { ?>
 	    <div id="headerimgs">
 		<div id="control" class="btn"></div>
@@ -82,40 +92,39 @@ global $options;
 	    </div>
 	     <?php } ?>
 	</div></header>  <!-- end: kopf -->
-      	<hr id="nachkopf" />  
+      	<hr id="nachkopf" />
 		<div id="main">  <!-- begin: main -->
-      		<div id="menu">  <!-- begin: menu -->	  
+      		<div id="menu">  <!-- begin: menu -->
 		        <div id="bereichsmenu">
-		         	<h2><a name="bereichsmenumarke" id="bereichsmenumarke">Navigation</a></h2>      
-				  
-			    <?php 
+		         	<h2><a name="bereichsmenumarke" id="bereichsmenumarke">Navigation</a></h2>
+
+			    <?php
                                 if ( has_nav_menu( 'primary' ) ) {
                                     wp_nav_menu( array( 'container' => 'ul',  'menu_class'  => 'menu',
-					'menu_id'         => 'navigation',  'theme_location' => 'primary', 'walker'  => '' ));      
+					'menu_id'         => 'navigation',  'theme_location' => 'primary', 'walker'  => '' ));
                                 } else { ?>
-                                    
-                                        <ul id="navigation" class="menu">      
+
+                                        <ul id="navigation" class="menu">
                                             <?php  wp_page_menu( array(
                                         'sort_column' => 'menu_order, post_title',
                                         'echo'        => 1,
-                                        'show_home'   => 1 ) ); ?>          
+                                        'show_home'   => 1 ) ); ?>
                                         </ul>
-                                    
+
                                 <?php  } ?>
-				
-				
+
+
 		        </div>
-			<?php if ( is_active_sidebar( 'kurzinfo-area' ) ) { 
-			    dynamic_sidebar( 'kurzinfo-area' ); 
+			<?php if ( is_active_sidebar( 'kurzinfo-area' ) ) {
+			    dynamic_sidebar( 'kurzinfo-area' );
 			 } ?>
 		</div>  <!-- end: menu -->
 
 		<?php get_sidebar(); ?>
-		
-		             
-	    	<div id="content">  <!-- begin: content -->
-	        <a name="contentmarke" id="contentmarke"></a>       
-    
-    
 
-	    
+
+	    	<div id="content">  <!-- begin: content -->
+	        <a name="contentmarke" id="contentmarke"></a>
+
+
+
